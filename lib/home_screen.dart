e210f83cff93411e3c077ea39169edc8380604f7
liupan <1206284818@qq.com>
2020-09-17 11:30:52 +0800

    modified: home page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:lavie/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'model/homelist.dart';
+import 'package:lavie/hotel_booking/hotel_home_screen.dart';
+// import 'model/homelist.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key key}) : super(key: key);
@@ -10,7 +11,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  List<HomeList> homeList = HomeList.homeList;
+  // List<HomeList> homeList = HomeList.homeList;
   AnimationController animationController;
   bool multiple = true;
 
@@ -57,50 +58,51 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
                         if (!snapshot.hasData) {
                           return const SizedBox();
                         } else {
-                          return GridView(
-                            padding: const EdgeInsets.only(
-                                top: 0, left: 12, right: 12),
-                            // ClampingScrollPhysics Android下的微光效果
-                            // BouncingScrollPhysics IOS下的弹性效果
-                            physics: const BouncingScrollPhysics(),
-                            scrollDirection: Axis.vertical,
-                            children: List<Widget>.generate(
-                              homeList.length,
-                              (int index) {
-                                final int count = homeList.length;
-                                final Animation<double> animation =
-                                    Tween<double>(begin: 0.0, end: 1.0).animate(
-                                  CurvedAnimation(
-                                    parent: animationController,
-                                    curve: Interval((1 / count) * index, 1.0,
-                                        curve: Curves.fastOutSlowIn),
-                                  ),
-                                );
-                                animationController.forward();
-                                return HomeListView(
-                                  animation: animation,
-                                  animationController: animationController,
-                                  listData: homeList[index],
-                                  callBack: () {
-                                    Navigator.push<dynamic>(
-                                      context,
-                                      MaterialPageRoute<dynamic>(
-                                        builder: (BuildContext context) =>
-                                            homeList[index].navigateScreen,
-                                      ),
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: multiple ? 2 : 1,
-                              mainAxisSpacing: 12.0,
-                              crossAxisSpacing: 12.0,
-                              childAspectRatio: 1.5,
-                            ),
-                          );
+                          return HotelHomeScreen();
+                          // return GridView(
+                          //   padding: const EdgeInsets.only(
+                          //       top: 0, left: 12, right: 12),
+                          //   // ClampingScrollPhysics Android下的微光效果
+                          //   // BouncingScrollPhysics IOS下的弹性效果
+                          //   physics: const BouncingScrollPhysics(),
+                          //   scrollDirection: Axis.vertical,
+                          //   children: List<Widget>.generate(
+                          //     homeList.length,
+                          //     (int index) {
+                          //       final int count = homeList.length;
+                          //       final Animation<double> animation =
+                          //           Tween<double>(begin: 0.0, end: 1.0).animate(
+                          //         CurvedAnimation(
+                          //           parent: animationController,
+                          //           curve: Interval((1 / count) * index, 1.0,
+                          //               curve: Curves.fastOutSlowIn),
+                          //         ),
+                          //       );
+                          //       animationController.forward();
+                          //       return HomeListView(
+                          //         animation: animation,
+                          //         animationController: animationController,
+                          //         listData: homeList[index],
+                          //         callBack: () {
+                          //           Navigator.push<dynamic>(
+                          //             context,
+                          //             MaterialPageRoute<dynamic>(
+                          //               builder: (BuildContext context) =>
+                          //                   homeList[index].navigateScreen,
+                          //             ),
+                          //           );
+                          //         },
+                          //       );
+                          //     },
+                          //   ),
+                          //   gridDelegate:
+                          //       SliverGridDelegateWithFixedCrossAxisCount(
+                          //     crossAxisCount: multiple ? 2 : 1,
+                          //     mainAxisSpacing: 12.0,
+                          //     crossAxisSpacing: 12.0,
+                          //     childAspectRatio: 1.5,
+                          //   ),
+                          // );
                         }
                       },
                     ),
@@ -172,59 +174,59 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   }
 }
 
-class HomeListView extends StatelessWidget {
-  const HomeListView(
-      {Key key,
-      this.listData,
-      this.callBack,
-      this.animationController,
-      this.animation})
-      : super(key: key);
+// class HomeListView extends StatelessWidget {
+//   const HomeListView(
+//       {Key key,
+//       this.listData,
+//       this.callBack,
+//       this.animationController,
+//       this.animation})
+//       : super(key: key);
 
-  final HomeList listData;
-  final VoidCallback callBack;
-  final AnimationController animationController;
-  final Animation<dynamic> animation;
+//   final HomeList listData;
+//   final VoidCallback callBack;
+//   final AnimationController animationController;
+//   final Animation<dynamic> animation;
 
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: animationController,
-      builder: (BuildContext context, Widget child) {
-        return FadeTransition(
-          opacity: animation,
-          child: Transform(
-            transform: Matrix4.translationValues(
-                0.0, 50 * (1.0 - animation.value), 0.0),
-            child: AspectRatio(
-              aspectRatio: 1.5,
-              child: ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: <Widget>[
-                    Image.asset(
-                      listData.imagePath,
-                      fit: BoxFit.cover,
-                    ),
-                    Material(
-                      color: Colors.transparent,
-                      child: InkWell(
-                        splashColor: Colors.grey.withOpacity(0.2),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(4.0)),
-                        onTap: () {
-                          callBack();
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnimatedBuilder(
+//       animation: animationController,
+//       builder: (BuildContext context, Widget child) {
+//         return FadeTransition(
+//           opacity: animation,
+//           child: Transform(
+//             transform: Matrix4.translationValues(
+//                 0.0, 50 * (1.0 - animation.value), 0.0),
+//             child: AspectRatio(
+//               aspectRatio: 1.5,
+//               child: ClipRRect(
+//                 borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+//                 child: Stack(
+//                   alignment: AlignmentDirectional.center,
+//                   children: <Widget>[
+//                     Image.asset(
+//                       listData.imagePath,
+//                       fit: BoxFit.cover,
+//                     ),
+//                     Material(
+//                       color: Colors.transparent,
+//                       child: InkWell(
+//                         splashColor: Colors.grey.withOpacity(0.2),
+//                         borderRadius:
+//                             const BorderRadius.all(Radius.circular(4.0)),
+//                         onTap: () {
+//                           callBack();
+//                         },
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
