8bef58cfda04f88787b154b6755c865cfd0cd8a4
liupan <1206284818@qq.com>
2020-09-17 17:54:41 +0800

    Add：idea card

--- lib/widgets/idea_card.dart
+++ lib/widgets/idea_card.dart
@@ -3,25 +3,17 @@ import 'package:flutter/material.dart';
 class IdeaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Flex(
-      direction: Axis.horizontal,
+    return Row(
       children: <Widget>[
-        Expanded(
-            flex: 1,
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://www.woolha.com/media/2020/03/eevee.png'),
-              // radius: 200,
-            )),
-        Expanded(
-            flex: 1,
-            child: Flex(
-              direction: Axis.vertical,
-              children: <Widget>[
-                Expanded(flex: 1, child: Text('潇洒的一条狗')),
-                Expanded(flex: 1, child: Text('工程师 3小时前'))
-              ],
-            ))
+        Container(
+          height: 60.0,
+          width: 60.0,
+          child: CircleAvatar(
+            backgroundImage:
+                NetworkImage('https://www.woolha.com/media/2020/03/eevee.png'),
+            radius: 40,
+          ),
+        ),
       ],
     );
   }
