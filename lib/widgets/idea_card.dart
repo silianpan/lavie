1446b8e2d32877ae9b564bd886a9a490ac9febd1
liupan <1206284818@qq.com>
2020-09-18 17:26:46 +0800

    Modified：idea card

--- lib/widgets/idea_card.dart
+++ lib/widgets/idea_card.dart
@@ -3,18 +3,53 @@ import 'package:flutter/material.dart';
 class IdeaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Container(
-          height: 60.0,
-          width: 60.0,
-          child: CircleAvatar(
-            backgroundImage:
-                NetworkImage('https://www.woolha.com/media/2020/03/eevee.png'),
-            radius: 40,
-          ),
+    return Container(
+      alignment: Alignment.topLeft,
+      child: Card(
+        margin: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://www.woolha.com/media/2020/03/eevee.png'),
+                      radius: 20,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Flex(
+                      direction: Axis.vertical,
+                      children: <Widget>[Text('潇洒的一条狗'), Text('攻城狮 3小时前')],
+                    )
+                  ],
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                ButtonBar(
+                  children: <Widget>[
+                    FlatButton(
+                      child: const Text('BUY TICKETS'),
+                      onPressed: () {/* ... */},
+                    ),
+                    FlatButton(
+                      child: const Text('LISTEN'),
+                      onPressed: () {/* ... */},
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
-      ],
+      ),
     );
   }
 }
