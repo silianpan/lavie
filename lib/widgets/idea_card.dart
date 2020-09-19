b94983892e4d380a9af0aa6893414d4038743359
liupan <1206284818@qq.com>
2020-09-19 16:34:21 +0800

    Learn Layout

--- lib/widgets/idea_card.dart
+++ lib/widgets/idea_card.dart
@@ -6,50 +6,62 @@ class IdeaCard extends StatelessWidget {
     return Container(
       alignment: Alignment.topLeft,
       child: Card(
-        margin: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(
+          margin: EdgeInsets.all(16.0),
+          child: Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                Column(
+                Row(
                   children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://www.woolha.com/media/2020/03/eevee.png'),
-                      radius: 20,
+                    Column(
+                      children: <Widget>[
+                        Container(
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://www.woolha.com/media/2020/03/eevee.png'),
+                            radius: 20,
+                          ),
+                        )
+                      ],
                     ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Flex(
-                      direction: Axis.vertical,
-                      children: <Widget>[Text('潇洒的一条狗'), Text('攻城狮 3小时前')],
+                    Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Column(
+                        children: <Widget>[
+                          Flex(
+                            direction: Axis.vertical,
+                            children: <Widget>[
+                              Text(
+                                '潇洒的一条狗',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15.0),
+                              ),
+                              Text(
+                                '攻城狮 3小时前',
+                                style: TextStyle(fontSize: 13.0),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     )
                   ],
-                )
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                ButtonBar(
-                  children: <Widget>[
-                    FlatButton(
-                      child: const Text('BUY TICKETS'),
-                      onPressed: () {/* ... */},
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: Wrap(children: <Widget>[
+                    Text(
+                      '看最美的风景，看最优秀的人，致自己，最优秀的自己',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 15.0),
                     ),
-                    FlatButton(
-                      child: const Text('LISTEN'),
-                      onPressed: () {/* ... */},
-                    ),
-                  ],
+                  ]),
                 ),
               ],
-            )
-          ],
-        ),
-      ),
+            ),
+          )),
     );
   }
 }
