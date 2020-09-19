e8c56cb56910b9e2e3d51be78cc403be621d75a0
liupan <1206284818@qq.com>
2020-09-19 23:34:56 +0800

    Learn Layout

--- lib/widgets/idea_card.dart
+++ lib/widgets/idea_card.dart
@@ -4,64 +4,73 @@ class IdeaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.topLeft,
-      child: Card(
-          margin: EdgeInsets.all(16.0),
-          child: Padding(
-            padding: EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Row(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.all(12.0),
+      color: Colors.red,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Container(
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://www.woolha.com/media/2020/03/eevee.png'),
+                      radius: 20,
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: Column(
                   children: <Widget>[
-                    Column(
+                    Flex(
+                      direction: Axis.vertical,
                       children: <Widget>[
-                        Container(
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(
-                                'https://www.woolha.com/media/2020/03/eevee.png'),
-                            radius: 20,
-                          ),
+                        Text(
+                          '潇洒的一条狗',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15.0),
+                        ),
+                        Text(
+                          '攻城狮 3小时前',
+                          style: TextStyle(fontSize: 13.0),
                         )
                       ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Column(
-                        children: <Widget>[
-                          Flex(
-                            direction: Axis.vertical,
-                            children: <Widget>[
-                              Text(
-                                '潇洒的一条狗',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 15.0),
-                              ),
-                              Text(
-                                '攻城狮 3小时前',
-                                style: TextStyle(fontSize: 13.0),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
                     )
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: Wrap(children: <Widget>[
-                    Text(
-                      '看最美的风景，看最优秀的人，致自己，最优秀的自己',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 15.0),
-                    ),
-                  ]),
-                ),
-              ],
-            ),
-          )),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: Wrap(children: <Widget>[
+              Text(
+                '看最美的风景，看最优秀的人，致自己，最优秀的自己',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15.0),
+              ),
+            ]),
+          ),
+          // GridView(
+          //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          //       crossAxisCount: 3, //横轴三个子widget
+          //       childAspectRatio: 1.0 //宽高比为1时，子widget
+          //       ),
+          //   children: <Widget>[
+          //     Icon(Icons.ac_unit),
+          //     Icon(Icons.airport_shuttle),
+          //     Icon(Icons.all_inclusive),
+          //     Icon(Icons.beach_access),
+          //     Icon(Icons.cake),
+          //     Icon(Icons.free_breakfast)
+          //   ],
+          // ),
+        ],
+      ),
     );
   }
 }
